588453a3cb7ba79d240e9873944821d94a4cd936
Takayuki Ota <takayukiota0905@gmail.com>
2019-04-20 19:38:36 +0200

    2_deploy add hoge

---
+++ migrations/2_deploy_takacoin.js
@@ -0,0 +1,8 @@
+var Takacoin = artifacts.require("./Takacoin.sol");
+
+module.exports = function(deployer){
+    var initialSupply = 1000;
+    deployer.deploy(Takacoin,initialSupply);
+}
+
+// # hoge
